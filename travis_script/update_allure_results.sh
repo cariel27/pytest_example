bb654d894de319a3223645eb10518f699c66083f
Cristian Ibanez <cibanez@belatrixsf.com>
2019-04-09 14:55:54 -0300

    allure script fix

--- travis_script/update_allure_results.sh
+++ travis_script/update_allure_results.sh
@@ -2,7 +2,7 @@
 TAS_ALLURE_RESULTS_PATH=/home/travis/build/cariel27/pytest_example/allure_results/
 ALLURE_RESULTS_REPO=/home/travis/build/cariel27/allure_results/results
 
-if test -f "$TAS_ALLURE_RESULTS_PATH/*.json"; then
+if test -f "/home/travis/build/cariel27/pytest_example/allure_results/*.json"; then
     cp -r "$TAS_ALLURE_RESULTS_PATH"/* "$ALLURE_RESULTS_REPO"
     cd /home/travis/build/cariel27/allure_results/
     git add .
