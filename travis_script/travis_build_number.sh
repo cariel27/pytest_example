5af6b6446802803bcf8a16ceb4db7607645944cf
Cristian Ibanez <cibanez@belatrixsf.com>
2019-04-10 12:10:38 -0300

    travis build number fix

--- travis_script/travis_build_number.sh
+++ travis_script/travis_build_number.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+TAS_PATH=/home/travis/build/cariel27/pytest_example/
+
+cd "$TAS_PATH"
 > travis_script/jenkins_set_build_number.sh
 echo "set BUILD_NUMBER="$TRAVIS_BUILD_NUMBER >> travis_script/jenkins_set_build_number.sh
 chmod +x travis_script/jenkins_set_build_number.sh
 git add travis_script/jenkins_set_build_number.sh
 git commit -m "Creating jenkins_set_build_number.sh"
-git push
+git push origin master
