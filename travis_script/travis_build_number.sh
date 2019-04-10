d3078c39a7bfe986470be7a4b38c98793316040f
Cristian Ibanez <cibanez@belatrixsf.com>
2019-04-10 12:33:59 -0300

    update and fix scripts for getting travis build number

--- travis_script/travis_build_number.sh
+++ travis_script/travis_build_number.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-TAS_PATH=/home/travis/build/cariel27/pytest_example/
+ALLURE_REPO_JENKINS_SCRIPT=/home/travis/build/cariel27/allure_results/jenkins_script
 
-cd "$TAS_PATH"
-> travis_script/jenkins_set_build_number.sh
-echo "set BUILD_NUMBER="$TRAVIS_BUILD_NUMBER >> travis_script/jenkins_set_build_number.sh
-chmod +x travis_script/jenkins_set_build_number.sh
-git add travis_script/jenkins_set_build_number.sh
+cd "ALLURE_REPO_JENKINS_SCRIPT"
+> jenkins_set_build_number.sh
+echo "set BUILD_NUMBER="$TRAVIS_BUILD_NUMBER >> jenkins_set_build_number.sh
+chmod +x jenkins_set_build_number.sh
+git add jenkins_set_build_number.sh
 git commit -m "Creating jenkins_set_build_number.sh"
-git push 'https://cariel27:P4ssw0rd!11@github.com/cariel27/pytest_example.git/' master
--- travis_script/update_allure_results.sh
+++ travis_script/update_allure_results.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+TAS_PATH=/home/travis/build/cariel27/pytest_example/
 TAS_ALLURE_RESULTS_PATH=/home/travis/build/cariel27/pytest_example/allure_results/
 ALLURE_RESULTS_REPO=/home/travis/build/cariel27/allure_results/results
 
@@ -10,4 +11,8 @@ touch readme.txt
 cp -r "$TAS_ALLURE_RESULTS_PATH"/* "$ALLURE_RESULTS_REPO"
 git add .
 git commit -m "updating allure results"
+
+# Getting Travis Build Number and Generating the script to update Jenkins.
+./travis_build_number.sh
+
 git push 'https://cariel27:P4ssw0rd!11@github.com/cariel27/allure_results.git/' master
