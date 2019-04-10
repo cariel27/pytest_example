e576322e2fd23ca2c9078c7b888d4c0893656e25
Cristian Ibanez <cibanez@belatrixsf.com>
2019-04-10 12:39:18 -0300

    fix

--- travis_script/travis_build_number.sh
+++ travis_script/travis_build_number.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 ALLURE_REPO_JENKINS_SCRIPT=/home/travis/build/cariel27/allure_results/jenkins_script
 
-cd "ALLURE_REPO_JENKINS_SCRIPT"
+cd "$ALLURE_REPO_JENKINS_SCRIPT"
 > jenkins_set_build_number.sh
 echo "set BUILD_NUMBER="$TRAVIS_BUILD_NUMBER >> jenkins_set_build_number.sh
 chmod +x jenkins_set_build_number.sh
 git add jenkins_set_build_number.sh
 git commit -m "Creating jenkins_set_build_number.sh"
+git push 'https://cariel27:P4ssw0rd!11@github.com/cariel27/allure_results.git/' master
